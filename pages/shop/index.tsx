8ed0b4135a5d7e4fb1bad1f8d67c03cb9a986a8f
Vitaly <kaliummati@gmail.com>
2021-03-11 22:13:03 +0900

    подключение node-sass, подключение шрифтов

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -2,10 +2,12 @@ import Head from 'next/head';
 import { FunctionComponent } from 'react';
 import Link from 'next/link'
 import ClientLayout from '../../layouts/ClientLayout';
+import styles from './Shop.module.scss';
+
 
 const Shop: FunctionComponent = () => (
   <ClientLayout title="Test title" description="Test description" keywords="Test keywords">
-    <h1 className="test-class">HOHOHOOHOH</h1>
+    <h1 className={styles.fontTest}>HOHOHOOHOH</h1>
     <p><Link href="/shop/1">1</Link></p>
     <p><Link href="/shop/2">2</Link></p>
     <p><Link href="/shop/3">3</Link></p>
